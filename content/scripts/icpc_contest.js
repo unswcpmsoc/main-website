bc00160c74fb2fdb152a5f8e1fcaa2232b0c676b
David Zhou <zhouyi021022@gmail.com>
2022-12-17 16:03:50 +1100

    add

--- content/scripts/icpc_contest.js
+++ content/scripts/icpc_contest.js
@@ -334,7 +334,7 @@ function loadingProgress() {
 
   document.getElementById("loading-bar").innerHTML = `
     <div class="progress">
-      <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-label="Animated striped example" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width: 60%"></div>
+      <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-label="Animated striped example" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width: 80%"></div>
     </div><br>
   `;
 }
